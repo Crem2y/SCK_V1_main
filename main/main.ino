2b2a1320755836e189b60f8bde5ec68f070e3772
Crem2y <31000852+Crem2y@users.noreply.github.com>
2023-01-15 17:10:57 +0900

    인터럽트에서 인터럽트를 해제하는 문제 수정

--- main/main.ino
+++ main/main.ino
@@ -7,7 +7,7 @@
 //#include "customcode.h"
 //#include "keydatas.h"
 //#include "constants.h"
-#include "i2c_master_falling.h"
+#include "i2c_master_interrupt.h"
 
 #define TIM_DISABLE TIMSK0&=(0<<OCIE0A)
 #define TIM_ENABLE TIMSK0|=(1<<OCIE0A)
@@ -22,57 +22,66 @@ void setup(void) {
   pinMode(LED_BUILTIN, OUTPUT);
 
   Serial.begin(115200);
-  while (!Serial);
+  delay(3000);
   
-  Serial.println("[ver] SCK V1");
-  Serial.println("[sys] waiting 5 seconds...");
-  delay(5000);
-  Serial.println("[sys] done!");
+  Serial.println("[ver] --SCK V1--");
+  Serial.print("[sys] waiting 5 seconds");
+  for(i=0; i<5; i++) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println("done!");
 
   // I2C set
   if(!I2C_init()) {
     Serial.println("[I2C] init error!");
     while(1);
   }
-  Serial.println("[I2C] init!");
 
   sei();
 
-  for(i=0x21; i<0x30; i++) {
-    Serial.print("[I2C] slave 0x");
+  Serial.println("[I2C] slave scanning...");
+  digitalWrite(LED_BUILTIN, HIGH);
+  for(i=0x20; i<0x70; i++) {
+    Serial.print("[I2C] 0x");
     Serial.print(i, HEX);
-    Serial.print(" checking...");
     if(I2C_check(i)) {
-      Serial.println(" ACK!");
+      Serial.print(" ACK! data : 0x");
+      if(I2C_reading_data[0] < 0x10) Serial.print('0');
+      Serial.println(I2C_reading_data[0], HEX);
     } else {
       Serial.println(" ERROR!");
     }
     delay(100);
   }
+  digitalWrite(LED_BUILTIN, LOW);
   Serial.println("[I2C] all slaves checked!\n");
   // I2C set end
-  Serial.println("[sys] waiting 5 seconds...");
-  delay(5000);
+  Serial.print("[sys] waiting 3 seconds");
+  for(i=0; i<3; i++) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println("done!");
 }
 
 //////////////////////////////// main loop start ////////////////////////////////
 void loop(void) {
   digitalWrite(LED_BUILTIN, HIGH);
+  
   while(Serial.available()) { //데이터가 오면
     uartString = Serial.readStringUntil('\n');
     Serial.println(uartString);
   }
 
-  I2C_write_byte(0x21);
-
-  I2C_reading_data[0] = 0xff;
   if(!I2C_read_byte(0x21)) {
     Serial.println("[I2C] error!");
   }
   while(I2C_is_communicating);
   Serial.print("[I2C] data = 0x");
+  if(I2C_reading_data[0] < 0x10) Serial.print('0');
   Serial.println(I2C_reading_data[0], HEX);
 
-  delay(1000);
+  delay(100);
 }
 //////////////////////////////// main loop end ////////////////////////////////
