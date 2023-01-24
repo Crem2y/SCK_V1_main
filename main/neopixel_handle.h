17d94c1c8381c15486ef6d19c8e8554c37829e54
yongjin kim <yongjin080322@gmail.com>
2023-01-24 13:28:13 +0900

    네오픽셀 하나만 초기화하는걸로 변경

--- main/neopixel_handle.h
+++ main/neopixel_handle.h
@@ -5,35 +5,35 @@
 
 #include "sck_key_datas.h"
 
-#define KEY_PIN  9  // neopixel data pin
-#define KEYS_NUM 74 // number of neopixels
-#define SIDE_PIN 8  // neopixel data pin
-#define SIDE_NUM 16 // number of neopixels
-#define P_BMAX   15 // PIXEL_BRIGHTMAX
+#define NEO_PIN 9     // neopixel data pin
+#define NEO_NUM 74+16 // number of neopixels
+#define P_BMAX  15    // PIXEL_BRIGHTMAX
 
-Adafruit_NeoPixel pixel_key  = Adafruit_NeoPixel(KEYS_NUM, KEY_PIN,  NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel pixel_side = Adafruit_NeoPixel(SIDE_NUM, SIDE_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel neopixel = Adafruit_NeoPixel(NEO_NUM, NEO_PIN,  NEO_GRB + NEO_KHZ800);
 
 void Neo_init(void);
 void Neo_loop(void);
 void Neo_off(void);
+void Neo_side_off(void);
 
 void Neo_init(void) {
-  pixel_key.begin();
-  pixel_side.begin();
+  neopixel.begin();
 }
 
 void Neo_loop(void) {
 
 
-  pixel_key.show();
-  pixel_side.show();
+  neopixel.show();
 }
 
 void Neo_off(void) {
-  pixel_key.fill(pixel_side.Color(0,0,0,0),0,0);
-  pixel_side.fill(pixel_side.Color(0,0,0,0),0,0);
+  neopixel.fill(neopixel.Color(0,0,0,0),0,0);
 
-  pixel_key.show();
-  pixel_side.show();
+  neopixel.show();
+}
+
+void Neo_side_off(void) {
+  neopixel.fill(neopixel.Color(0,0,0,0),74,0);
+
+  neopixel.show();
 }
